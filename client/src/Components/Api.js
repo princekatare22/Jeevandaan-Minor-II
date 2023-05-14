9d69ee02d2026e8668a8fbe452d985accbbba108
princekatare22 <princekatare22@gmail.com>
2023-05-14 19:48:10 +0530

    final commit

--- client/src/Components/Api.js
+++ client/src/Components/Api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3177/";
+const baseURL = "https://jeevandaanbackend.onrender.com/";
 
 export default axios.create({ baseURL: baseURL });
--- client/src/Components/context/AuthContext.js
+++ client/src/Components/context/AuthContext.js
@@ -8,7 +8,11 @@ function AuthContextProvider(props) {
   const [user, setUser] = useState([]);
 
   async function getLoggedIn() {
-    const loggedInRes = await axios.get("http://localhost:3177/auth/loggedIn", { withCredentials: true });
+    //  const loggedInRes = await axios.get("http://localhost:3177/auth/loggedIn", { withCredentials: true });
+    const loggedInRes = await axios.get(
+      "https://jeevandaanbackend.onrender.com/auth/loggedIn",
+      { withCredentials: true }
+    );
     setLoggedIn(loggedInRes.data.auth);
     setUser(loggedInRes.data.user);
   }
