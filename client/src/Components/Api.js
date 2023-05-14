9c038754452e36647680c8d5877750dd734b8392
princekatare22 <princekatare22@gmail.com>
2023-05-14 22:02:37 +0530

    deployment phase

--- client/src/Components/Api.js
+++ client/src/Components/Api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://jeevandaanbackend.onrender.com";
+const baseURL = "https://jeevandaanbackend.onrender.com/";
+// const baseURL = "http://localhost:3177/";
 
 export default axios.create({ baseURL: baseURL });
