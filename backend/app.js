791b24e8aeac5f8746fd6e42587242993ac03888
princekatare22 <princekatare22@gmail.com>
2023-05-14 20:00:39 +0530

    final commit

--- backend/app.js
+++ backend/app.js
@@ -11,13 +11,27 @@ dotenv.config();
 
 app.use(cookieParser());
 app.use(express.json());
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000", "https://jeevandaan.onrender.com"],
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://jeevandaan.onrender.com"],
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "x-csrf-token",
+      "Access-Control-Allow-Origin",
+    ],
     credentials: true,
+    maxAge: 5000,
+    exposedHeaders: ["*", "Authorization", "https://jeevandaan.onrender.com"],
   })
 );
-
 mongoose.connect(
   "mongodb+srv://princekatare22:prince@2207@cluster0.ztt9urk.mongodb.net/?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
