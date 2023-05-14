3ac7803ce5217f3f5f6296018dfb1bcc5f8f3484
princekatare22 <princekatare22@gmail.com>
2023-05-14 19:34:44 +0530

    final commit

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://jeevandaanbackend.onrender.com"],
     credentials: true,
   })
 );
